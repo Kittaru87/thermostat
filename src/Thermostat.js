b23a70bd65c7fe16d3a4102a428e7cfe59578ea1
Sophia Bell <Kittaru87@gmail.com>
2020-03-18 14:26:51 +0000

    adding energy Usage + tests

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat() {
   this.MINIMUM_TEMP = 10;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
   this.MAX_TEMP_PSM_ON = 25;
   this.MAX_TEMP_PSM_OFF = 32;
   this.DEFAULT_TEMP = 20;
@@ -52,4 +53,13 @@ Thermostat.prototype.down = function(){
 
 Thermostat.prototype.resetTemperature = function(){
   this.temperature = this.DEFAULT_TEMP;
+}
+
+Thermostat.prototype.energyUsage = function(){
+  if(this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT ){
+    return 'low-usage';
+  } else if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_TEMP_PSM_ON){
+    return 'medium-usage';
+  }
+  return 'high-usage';
 }
